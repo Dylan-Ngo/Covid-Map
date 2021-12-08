51774248d7adce123ddae5023bdc6cc711819033
Dylan Ngo <dngo97@csu.fullerton.edu>
2021-12-07 22:55:29 -0800

    Fix links

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
             <a href="https://coviddashboard-team2DE.netlify.app/">COUNTRY INFO</a>
           </li>
           <li>
-            <a href="https://www.vaccines.gov/search/" target="_blank">GET VACCINE</a>
+            <a href="https://www.vaccines.gov/search/" target="_blank">GET VACCINES</a>
           </li>
         </ul>
       </Container>
